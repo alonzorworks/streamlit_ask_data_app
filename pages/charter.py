7f5d277f8de0c4d68280b3cbda3b6924b7ab39bc
alonzo-ahead <132846809+alonzo-ahead@users.noreply.github.com>
2023-08-24 10:04:14 -0500

    Hopefully allows users to use uploaded dataset.

--- pages/charter.py
+++ pages/charter.py
@@ -14,14 +14,13 @@ from pathlib import Path
 import os
 import sys #df = pd.read_csv(df)
 from streamlit_folium import folium_static
-import altair_viewer
+from a_robot_charter import df_export
  #fig = "I am here"
 df = pd.read_csv(r"pages/dataset.csv") #df = pd.read_csv(r"writable_files\dataset.csv")
 # Need to Assign the df2 to df because that is the default dataframe that GPT deals with 
 #Can use a downloaded file to obtain the CSV  # Tries to import the uploaded file if possible, if it is possible it is overwritten
-try:
-    from a_robot_charter import df_export
+if df_export:
     df = pd.read_csv(df_export)
-finally:
-    pass
+else:
+    df = df = pd.read_csv(r"pages/dataset.csv")
     
