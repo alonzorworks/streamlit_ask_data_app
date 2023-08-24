a46e6166d89d2d0dde5247c672461be847c754a8
alonzo-ahead <132846809+alonzo-ahead@users.noreply.github.com>
2023-08-24 10:25:10 -0500

    Have a third party file import the needed dataframe.

--- pages/charter.py
+++ pages/charter.py
@@ -14,13 +14,13 @@ from pathlib import Path
 import os
 import sys #df = pd.read_csv(df)
 from streamlit_folium import folium_static
-from pages.a_robot_charter import df_export
+from pages.z_import import df_export2
  #fig = "I am here"
 df = pd.read_csv(r"pages/dataset.csv") #df = pd.read_csv(r"writable_files\dataset.csv")
 # Need to Assign the df2 to df because that is the default dataframe that GPT deals with 
 #Can use a downloaded file to obtain the CSV  # Tries to import the uploaded file if possible, if it is possible it is overwritten
-if df_export:
-    df = pd.read_csv(df_export)
+if df_export2:
+    df = pd.read_csv(df_export2)
 else:
     df = df = pd.read_csv(r"pages/dataset.csv")
     
---
+++ pages/z_import.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from pages.a_robot_charter import df_export
+
+df_export2 =df_export
